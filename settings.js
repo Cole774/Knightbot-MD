57f1fcca41fa08a236662da055524c2e696be731
Cole774 <abrahambenjmin152@gmail.com>
2025-03-01 00:21:57 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'Abraham cole', // Your name
-  ownerNumber: '2348144706835', //Your number
+  ownerNumber: '2349165218091', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
